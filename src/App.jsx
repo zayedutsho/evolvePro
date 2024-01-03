bf04c763913aac9169b14974e950dbf22c0b8fdd
TaamZID <tamjidalam00@gmail.com>
2024-01-03 15:32:41 +0600

    visitor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "../src/Comp/Hero";
 import Banner from "./Comp/Banner";
+import Visitor from "./Comp/Visitor";
 
 function App() {
   return (
     <>
       <Hero></Hero>
       <Banner />
+      <Visitor />
     </>
   );
 }
--- src/Comp/Visitor.jsx
+++ src/Comp/Visitor.jsx
@@ -1,5 +1,15 @@
 const Visitor = () => {
-  return <div></div>;
+  return (
+    <div className="pt-[150px] pb-[80px] text-center font-['Poppins']">
+      <h1 className="text-[#9A9AB5] lg:text-[20px] lg:font-medium">
+        Are You Ready to...
+      </h1>
+      <div className="text-[#292930] lg:text-[71px] lg:font-medium lg:mt-[24px] leading-none">
+        <h1>Turn Every Website Visitor into</h1>
+        <h1>a Potential Customer?</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Visitor;
