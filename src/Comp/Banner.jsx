ef02c45dc11f1c6170ecf4557abb7e161c340041
TaamZID <tamjidalam00@gmail.com>
2024-01-03 15:05:50 +0600

    banner-update

---
+++ src/Comp/Banner.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-irregular-whitespace */
+import bannerImage from "../assets/bannerImage.png";
+const Banner = () => {
+  return (
+    <div className="py-[150px] px-[250px] bg-[linear-gradient(180deg,_#dedede00_0%,_#174aff4d_100%)] ">
+      <div className="flex items-center justify-center">
+        <img src={bannerImage} alt="bannerImage" />
+      </div>
+      <div>
+        <h1 className="text-center lg:text-[32px] font-['Poppins'] font-bold text-[#fff]">
+          <span className="py-[29px] px-[44px] border-b-[3px] border-[#0084CB] shadow-[0_20px_30px_0px_rgba(0, 93, 114, 0.25)] bg-[linear-gradient(180deg,_#36DAFE_0%,_#129CE9_100%)] rounded-[20px]">
+            Start Your Free Trial Today 》
+          </span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
