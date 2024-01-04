9c495685b5c2887f25fce769fb28c5cbe54507b0
TaamZID <tamjidalam00@gmail.com>
2024-01-04 22:05:36 +0600

    meet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Hero from "../src/Comp/Hero";
 import Banner from "./Comp/Banner";
+import Connected from "./Comp/Connected";
+import Meet from "./Comp/Meet";
 import Visitor from "./Comp/Visitor";
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
       <Hero></Hero>
       <Banner />
       <Visitor />
+      <Meet />
+      <Connected />
     </>
   );
 }
