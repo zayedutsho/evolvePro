134d561f4a1647b22f7f8887b9c14df279f56742
zayed utsho <zayedutsho@example.com>
2024-01-04 21:42:38 +0600

    faq changed

--- src/Comp/Faq.jsx
+++ src/Comp/Faq.jsx
@@ -3,17 +3,27 @@ import { useState } from "react";
 const Faq = () => {
   const faqItems = [
     {
-      question: "What is Flowbite?",
+      question: "Can Magnetic Story Selling work for MY business?",
       answer:
         "Flowbite is an open-source library of interactive components built on top of Tailwind CSS.",
     },
     {
-      question: "Is there a Figma file available?",
+      question: "Why is ‘Magnetic Story Selling’ priced at $1,997?",
       answer:
         "Yes, Flowbite is first conceptualized and designed using the Figma software.",
     },
     {
-      question: "How can I get started?",
+      question: "What if ‘Magnetic Story Selling’ doesn’t help me?",
+      answer:
+        "Check out the documentation on the official website for a guide on getting started.",
+    },
+    {
+      question: "When will my copy of ‘Magnetic Story Selling’ get to me?",
+      answer:
+        "Check out the documentation on the official website for a guide on getting started.",
+    },
+    {
+      question: "Where can I order ‘Magnetic Story Selling’?",
       answer:
         "Check out the documentation on the official website for a guide on getting started.",
     },
@@ -27,35 +37,43 @@ const Faq = () => {
     };
 
     return (
-      <div className="mb-4 border-b p-8">
+      <div className="mb-4 border-b">
         <div
-          className="flex justify-between items-center cursor-pointer"
+          className={`flex justify-between items-center cursor-pointer ${
+            isOpen ? "w-full" : "max-w-[1241px]"
+          }`}
           onClick={toggleAccordion}
         >
-          <div className="flex items-center">
-            <span className="mr-2 text-gray-500">{num}.</span>
-            <h2 className="text-lg font-semibold text-lg font-">{question}</h2>
+          <div className="flex items-center space-x-5">
+            <span className="text-gray-500">{num}.</span>
+            <h2 className="text-lg font-semibold text-center text-nowrap ">
+              {question}
+            </h2>
           </div>
-          <span className="w-8 h-8 text-gray-500 inline-block rounded-full border border-purple-500   items-center justify-center flex">
+          <span className="w-8 h-8 text-gray-500 inline-block rounded-full border border-purple-500 items-center justify-center flex ">
             {isOpen ? "-" : "+"}
           </span>
         </div>
-        {isOpen && <p className="mt-2">{answer}</p>}
+        {isOpen && <p className="mt-2  text-nowrap text-start ">{answer}</p>}
       </div>
     );
   };
 
   return (
-    <div className="max-w-md mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4 text-center  text-7xl">FAQ</h1>
-      {faqItems.map((item, index) => (
-        <AccordionItem
-          key={index}
-          num={index + 1}
-          question={item.question}
-          answer={item.answer}
-        />
-      ))}
+    <div className="py-[150px] px-[250px] bg-[linear-gradient(180deg,_#dedede00_0%,_#174aff4d_100%)]">
+      <div className="max-w-[1241px] mx-auto">
+        <h1 className="text-3xl font-bold mb-4 text-center text-7xl text-black mb-[92px]">
+          FAQ
+        </h1>
+        {faqItems.map((item, index) => (
+          <AccordionItem
+            key={index}
+            num={index + 1}
+            question={item.question}
+            answer={item.answer}
+          />
+        ))}
+      </div>
     </div>
   );
 };
