f916455f946c4aa5924d4b1326cff3ba7f86f59f
zayed utsho <zayedutsho@example.com>
2024-01-05 18:16:54 +0600

    vdo added

--- src/Comp/Banner.jsx
+++ src/Comp/Banner.jsx
@@ -1,13 +1,21 @@
 /* eslint-disable no-irregular-whitespace */
-import bannerImage from "../assets/bannerImage.png";
+// import bannerImage from "../assets/bannerImage.png";
+import video from "../assets/vdo.mp4";
 const Banner = () => {
   return (
     <div className="py-[150px] px-[250px] bg-[linear-gradient(180deg,_#dedede00_0%,_#174aff4d_100%)] ">
       <div className="flex items-center justify-center">
-        <img src={bannerImage} alt="bannerImage" />
+        {/* <img src={bannerImage} alt="bannerImage" /> */}
+        <video
+          src={video}
+          width="1200"
+          height="1200"
+          controls
+          className="border rounded-lg"
+        />
       </div>
       <div>
-        <h1 className="text-center lg:text-[32px] font-['Poppins'] font-bold text-[#fff]">
+        <h1 className="text-center lg:text-[32px] font-['Poppins'] font-bold text-[#fff]  mt-[100px]">
           <span className="py-[29px] px-[44px] border-b-[3px] border-[#0084CB] shadow-2xl bg-[linear-gradient(180deg,_#36DAFE_0%,_#129CE9_100%)] rounded-[20px]">
             Start Your Free Trial Today 》
           </span>
