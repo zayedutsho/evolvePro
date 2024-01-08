d80563bf5b92657c3c5b7122e23acc0c52af39a5
taamzid <tamjidalam00@gmail.com>
2024-01-09 00:09:07 +0600

    more-responsive

--- src/Comp/Connected.jsx
+++ src/Comp/Connected.jsx
@@ -4,22 +4,30 @@ import RightArrow from "../assets/RightArrow.svg";
 
 const Connected = () => {
   return (
-    <div className="pt-[100px] pb-[120px] relative">
-      <div className="text-center lg:text-[20px] font-normal text-[#9A9AB5] font-['Sharp-Grotesk-Medium']">
+    <div className="pt-[80px] lg:pt-[100px] pb-[80px] lg:pb-[120px] relative">
+      <div className="px-[16px] block lg:hidden text-center text-[14px] lg:text-[20px] font-normal text-[#9A9AB5] font-['Sharp-Grotesk-Medium']">
+        <h1>
+          In today’s fast-paced digital world, the difference between thriving
+          and merely surviving in business often comes down to one critical element:</h1>
+      </div>
+      <div className="hidden lg:block text-center lg:text-[20px] font-normal text-[#9A9AB5] font-['Sharp-Grotesk-Medium']">
         <h1>
           In today’s fast-paced digital world, the difference between thriving
           and merely
         </h1>
         <h1>surviving in business often comes down to one critical element:</h1>
       </div>
-      <div className="lg:mt-[20px] text-center lg:text-[50px] font-medium leading-[58px] font-['Sharp-Grotesk-Medium']">
+      <div className="block lg:hidden px-[16px] py-[24px] text-[28px] text-center leading-[32px] font-['Sharp-Grotesk-Medium']">
+        <h1>Meet EvolvePro.AI - The Revolutionary Chatbot Transforming How Businesses Connect, Communicate, and Convert Online</h1>
+      </div>
+      <div className="hidden lg:block lg:mt-[20px] text-center lg:text-[50px] font-medium leading-[58px] font-['Sharp-Grotesk-Medium']">
         <h1> Staying connected with your</h1>
         <h1>prospects around the clock.</h1>
       </div>
       <div className="lg:flex items-center justify-center lg:mt-[80px]">
         <img src={connectedImage} alt="connectedImage" />
       </div>
-      <div className="absolute top-[38%] left-[19%] font-['Poppins'] overflow-hidden">
+      <div className="hidden lg:block absolute top-[38%] left-[19%] font-['Poppins'] overflow-hidden">
         <div>
           <h1 className="text-[#292930] text-[40px] font-medium">
             Why EvolvePro.AI?
--- src/Comp/know.jsx
+++ src/Comp/know.jsx
@@ -2,12 +2,22 @@
 /* eslint-disable react/no-unescaped-entities */
 const know = () => {
   return (
-    <div className="lg:flex flex-col lg:pt-[160px] lg:pl-[240px] bg-[url('./assets/bg-know-mobile.svg')] lg:bg-[url('./assets/knowledgeImage.svg')] bg-[length:370px_600px] min-h-[600px] lg:min-h-[1970px] bg-contain lg:bg-cover">
-      <div className="text-[#222222] lg:text-[71px] lg:leading-[71px] font-semibold font-['Sharp-Grotesk-Medium']">
-        <h1 className="hidden lg:block">EvolvePro.AI</h1>
+    <div className="lg:flex flex-col lg:pt-[160px] lg:pl-[240px] bg-[url('./assets/bgk.svg')] lg:bg-[url('./assets/knowledgeImage.svg')] min-h-[550px] lg:min-h-[1970px] bg-cover lg:bg-cover">
+      <div className="text-[#222222] text-[28px] lg:text-[71px] lg:leading-[71px] font-semibold font-['Sharp-Grotesk-Medium']">
+        <h1 className="text-center lg:text-start">EvolvePro.AI</h1>
       </div>
-      <div className="hidden lg:mt-[70px]">
-        <h1 className="text-start lg:text-[16px] font-['Poppins'] font-bold text-[#fff]">
+      <div className="mt-[32px] lg:mt-[70px]">
+        <h1 className="block lg:hidden text-center lg:text-[16px] font-['Poppins'] font-bold text-[#fff]">
+          <span className="py-[20px] lg:py-[30px] px-[32px] lg:px-[36px] border-b-[3px] border-[#0084CB] shadow-2xl bg-[linear-gradient(180deg,_#36DAFE_0%,_#129CE9_100%)] rounded-[20px]">
+          Start Your Free Trial Today 》
+          </span>
+          <div className="text-start block lg:hidden text-[20px] text-[#292930] px-[50px] mt-[50px] font-bold leading-[26px]">
+            <h1 >Transform</h1>
+            <h1>Your Website</h1>
+            <h1>Today!</h1>
+          </div>
+        </h1>
+        <h1 className="hidden lg:block text-start lg:text-[16px] font-['Poppins'] font-bold text-[#fff]">
           <span className="py-[30px] px-[36px] border-b-[3px] border-[#0084CB] shadow-2xl bg-[linear-gradient(180deg,_#36DAFE_0%,_#129CE9_100%)] rounded-[20px]">
             Start Your Free Trial – Transform Your Website Today!
           </span>
